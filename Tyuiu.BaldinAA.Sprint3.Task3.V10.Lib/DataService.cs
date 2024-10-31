3a53e499b3fc853cfce2f48a08cae0e5dc811c6d
Tyuiu.BaldinAA@outlook.com <artyombaldin94438@gmail.com>
2024-10-31 22:34:39 +0500

    Task3

---
+++ Tyuiu.BaldinAA.Sprint3.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BaldinAA.Sprint3.Task3.V10.Lib
+{
+    public class DataService : ISprint3Task3V10
+    {
+        public string DeleteCharInString(string value, char item)
+        {
+
+                foreach (char i in value)
+                {
+                    if (i == item)
+                    {
+                        value = value.Replace(item.ToString(), "");
+                    }
+                }
+                return value;
+            }
+        }
+    };
+        
+    
+
---
+++ Tyuiu.BaldinAA.Sprint3.Task3.V10/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BaldinAA.Sprint3.Task3.V10.Lib;
+namespace Tyuiu.BaldinAA.Sprint3.Task3.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Балдин А. А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Цикл foreach                                                      *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Балдин Артём Александрович | ИБКСб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach удалить из строки все буквы p в строке:          *");
+            Console.WriteLine("* gdfppf vfppt p                                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            string value = "gdfppf vfppt p";
+            char item = 'p';
+            Console.WriteLine($" Строка = {value}.\n Буква = {item}.                                      ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.DeleteCharInString(value, item);
+            Console.WriteLine(res); ;
+        }
+    }
+}
